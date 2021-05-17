621e27bf74b4253d48b4a9e177d2541082716223
Azharuddin Arrosis <azharuddin.arrosis@machtwatch.co.id>
2021-05-17 21:19:28 +0700

    controller & models

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/azharoce/isirumahid
 
 go 1.15
 
-require github.com/360EntSecGroup-Skylar/excelize v1.4.1 // indirect
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.12
+)
+
--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"github.com/azharoce/gin-bookstore/controllers"
+	"github.com/azharoce/gin-bookstore/models"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello, playground")
+	r := gin.Default()
+
+	// Connect to database
+	models.ConnectDatabase()
+
+	// Routes
+	r.GET("/books", controllers.FindBooks)
+	r.GET("/books/:id", controllers.FindBook)
+	r.POST("/books", controllers.CreateBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	// Run the server
+	r.Run()
 }
